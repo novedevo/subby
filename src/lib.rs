665e370d234cdbc22eb2173922f857a0b0f6158a
Devon Sawatsky <novedevo@gmail.com>
2022-04-13 08:43:34 -0700

    simplify by using external crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,95 +1,55 @@
-pub mod builder;
 mod net;
 
 use anyhow::Result;
-use hyper::client::connect::HttpConnector;
-use hyper_rustls::HttpsConnector;
-use oauth2::{storage::TokenInfo, AccessToken};
-use yup_oauth2 as oauth2;
 
 pub struct PubSub {
     project_id: String,
-    auth_provider: AuthProvider,
+    auth: gcp_auth::AuthenticationManager,
+    client: reqwest::Client,
 }
 
 impl PubSub {
-    pub async fn topic(self, topic: String) -> Result<Topic> {
-        Ok(Topic {
-            project_id: self.project_id,
-            token: self.auth_provider.token().await?,
-            auth_provider: self.auth_provider,
-            topic,
+    pub async fn new() -> Result<Self> {
+        let auth = gcp_auth::AuthenticationManager::new().await?;
+        Ok(Self {
+            project_id: auth.project_id().await?,
+            auth,
             client: reqwest::Client::new(),
+        })
+    }
+    pub fn topic(&self, topic: String) -> Result<Topic<'_>> {
+        Ok(Topic {
+            project_id: &self.project_id,
+            auth: &self.auth,
+            topic,
+            client: &self.client,
         })
     }
 }
 
-pub struct Topic {
-    project_id: String,
-    auth_provider: AuthProvider,
-    token: AccessToken,
+pub struct Topic<'a> {
+    project_id: &'a str,
+    auth: &'a gcp_auth::AuthenticationManager,
     topic: String,
-    client: reqwest::Client,
+    client: &'a reqwest::Client,
 }
 
-impl Topic {
-    pub async fn publish<S>(&mut self, message: S) -> Result<String>
+impl Topic<'_> {
+    pub async fn publish<S>(&mut self, message: &S) -> Result<String>
     where
         S: serde::Serialize,
     {
-        if self.token.is_expired() {
-            self.token = self.auth_provider.token().await?;
-        }
         let res = net::publish(
-            &self.client,
+            self.client,
             &self.topic,
-            &self.project_id,
-            self.token.as_str(),
-            serde_json::to_string(&message)?,
+            self.project_id,
+            self.auth
+                .get_token(&["https://www.googleapis.com/auth/pubsub"])
+                .await?
+                .as_str(),
+            message,
         )
         .await?;
         Ok(res)
-    }
-}
-
-type Authenticator = oauth2::authenticator::Authenticator<HttpsConnector<HttpConnector>>;
-
-enum AuthProvider {
-    Gce(reqwest::Client),
-    SA(Authenticator),
-}
-
-impl AuthProvider {
-    async fn token(&self) -> Result<AccessToken> {
-        let token: AccessToken = match self {
-            Self::Gce(client) => {
-                serde_json::from_str::<GceResponse>(&net::gce_token(client).await?)?.into()
-            }
-            Self::SA(auth) => {
-                auth.token(&["https://www.googleapis.com/auth/pubsub"])
-                    .await?
-            }
-        };
-        Ok(token)
-    }
-}
-
-#[derive(serde::Deserialize)]
-struct GceResponse {
-    access_token: String,
-    expires_in: i64,
-}
-
-impl From<GceResponse> for AccessToken {
-    fn from(resp: GceResponse) -> Self {
-        TokenInfo {
-            access_token: resp.access_token,
-            expires_at: Some(
-                time::OffsetDateTime::now_utc() + time::Duration::seconds(resp.expires_in),
-            ),
-            id_token: None,
-            refresh_token: None,
-        }
-        .into()
     }
 }
--- src/net.rs
+++ src/net.rs
@@ -1,10 +1,13 @@
-pub async fn publish(
+pub async fn publish<S>(
     client: &reqwest::Client,
     topic: &str,
     project: &str,
     token: &str,
-    body: String,
-) -> reqwest::Result<String> {
+    body: &S,
+) -> reqwest::Result<String>
+where
+    S: serde::Serialize,
+{
     client
         .post(format!(
             "https://pubsub.googleapis.com/v1/projects/{}/topics/{}:publish",
@@ -13,34 +16,9 @@ pub async fn publish(
         .header("User-Agent", "subby_rs/0.1.0")
         .header("Authorization", format!("Bearer {}", token))
         .header("Content-Type", "application/json")
-        //todo: add content length and content type, if it fails
+        //todo: add content length, if it fails
         //todo: make this grpc
-        .body(body)
-        .send()
-        .await?
-        .text()
-        .await
-}
-
-pub async fn is_on_gce(client: &reqwest::Client) -> bool {
-    gce_project_id(client).await.is_ok()
-}
-
-pub async fn gce_token(client: &reqwest::Client) -> reqwest::Result<String> {
-    metadata(client, "instance/service_accounts/default/token").await
-}
-pub async fn gce_project_id(client: &reqwest::Client) -> reqwest::Result<String> {
-    metadata(client, "project/project_id").await
-}
-
-async fn metadata(client: &reqwest::Client, path: &str) -> reqwest::Result<String> {
-    client
-        .get(format!(
-            "http://metadata.google.internal/computeMetadata/v1/{}",
-            path
-        ))
-        .header("User-Agent", "subby_rs/0.1.0")
-        .header("Metadata-Flavor", "Google")
+        .json(body)
         .send()
         .await?
         .text()
